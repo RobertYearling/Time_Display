401c9b86e9227293d89598ecf4baccb2c67bdc6f
Robert Yearling <laserguy@mac.com>
2020-12-08 13:04:11 -0600

    all files uploaded

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TimeDisplay.Controllers     //be sure to use your own project's namespace!
+{
+    public class HomeController : Controller   //remember inheritance??
+    {
+        //for each route this controller is to handle:
+        [HttpGet]       //type of request
+        [Route("")]     //associated route string (exclude the leading /)
+        public ViewResult Index()
+        {
+            ViewBag.CreatedOn = DateTime.Now;
+            return View();
+        }
+    }
+}
